367fc0a0eb9c9a3752a52770063e7c408ad574b6
Jingshuai-cloud <xujingshuai@gmail.com>
2020-05-26 16:40:01 +1000

    initial commit

---
+++ src/main/java/com/mysql/mysqlspringboot/MysqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.mysqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysql/mysqlspringboot/MysqlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.mysqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
